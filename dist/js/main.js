4afcfecf5db3fb3d6cc9d8f2026e2682e24c5f88
Vlad <cpu.cpu@hotmail.com>
2018-12-01 00:10:52 +0100

    sidebar menu updates

---
+++ dist/js/main.js
@@ -0,0 +1,42 @@
+// find DOM items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const body = document.getElementsByTagName("body");
+
+// initial state
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
+
+// close the menu on click away
+window.addEventListener("mouseup", function(event) {
+  if (event.target != menu && event.target.parentNode != menu) {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    this.console.showMenu = false;
+  }
+});
