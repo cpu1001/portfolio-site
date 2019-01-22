abf2a27a8a61dbe369947cb015d6d805d4459bf1
Vlad <cpu.cpu@hotmail.com>
2019-01-22 23:43:44 +0100

    Portfolio screens added, IE grid fixes added, menu fixed in IE.

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,8 +5,8 @@ var app = new Vue({
                 name: 'Easy BTC Invest',
                 img: [
                     'img/portfolio/btc-01.jpg',
-                    'img/portfolio/bl-01.jpg',
-                    'img/portfolio/mn-01.jpg',
+                    'img/portfolio/btc-02.jpg',
+                    'img/portfolio/btc-03.jpg'
 
                 ],
                 description: 'Cryptocurrency is a fast growing market so to stay ahead of the curve USI-TECH wanted to offer a new service. I helped USI-TECH build a responsive, fast and flexible website for their BTC package investment program. '
@@ -15,21 +15,25 @@ var app = new Vue({
                 name: 'Botan Labs - Liver Extra',
                 img: [
                     'img/portfolio/bl-01.jpg',
-                    'img/portfolio/bl-02.jpg'
+                    'img/portfolio/bl-02.jpg',
+                    'img/portfolio/bl-03.jpg'
                 ],
                 description: 'Botan Labs hired me to come up with a complete look and feel for the site to show off their latest liver lealth product. '
             },
             {
                 name: 'Botan Labs - Moony600',
                 img: [
-                    'img/portfolio/mn-01.jpg'
+                    'img/portfolio/mn-01.jpg',
+                    'img/portfolio/mn-02.jpg',
+                    'img/portfolio/mn-03.jpg'
                 ],
                 description: 'Botan Labs needed a website and brand guidelines for their new dietary supplement. I worked with their back end dev team and delivered some awesome looking shopping cart pages '
             },
             {
                 name: 'Botan Labs - Curcumin',
                 img: [
-                    'img/portfolio/cc-01.jpg'
+                    'img/portfolio/cc-01.jpg',
+                    'img/portfolio/cc-02.jpg'
                 ],
                 description: 'I was hired to do the design and front end development for new product by Botan Labs. I worked with their dev team to incorporate custom built shopping cart features and the result was pretty awesome.'
             },
@@ -40,12 +44,14 @@ var app = new Vue({
                     'img/portfolio/mdc-01.jpg',
                     'img/portfolio/mdc-03.jpg'
                 ],
-                description: 'DiamondProtect is a High-Tech Glasses Protection suitable for all types of glasses. I was hired to design the new look for their website to show off their display and glasses protection products.'
+                description: 'MDC is a social network for healthy weight loss. I was doing most of UI / UX design work and concept design for various site and mobile app features.   '
             },
             {
                 name: 'Diamond Protect',
                 img: [
-                    'img/portfolio/dp-01.jpg'
+                    'img/portfolio/dp-01.jpg',
+                    'img/portfolio/dp-02.jpg',
+                    'img/portfolio/dp-03.jpg'
                 ],
                 description: 'DiamondProtect is a High-Tech Glasses Protection suitable for all types of glasses. I was hired to design the new look for their website to show off their display and glasses protection products.'
             }
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,19 @@
+"use strict";
+
 // Select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+var menuBtn = document.querySelector(".menu-btn");
+var menu = document.querySelector(".menu");
+var menuNav = document.querySelector(".menu-nav");
+var menuBranding = document.querySelector(".menu-branding");
 
 // Set Initial State Of Menu
-let showMenu = false;
+var showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
-navItems.forEach(item => item.addEventListener("click", toggleMenu));
+navItems.forEach(function (item) {
+  return item.addEventListener("click", toggleMenu);
+});
 
 function toggleMenu() {
   if (!showMenu) {
@@ -18,7 +21,6 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
@@ -27,34 +29,26 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
   }
 }
 
-const pItems = document.querySelectorAll(".p-item");
-const pClicks = document.querySelectorAll(".pclick");
+var pItems = document.querySelectorAll(".p-item");
+var pClicks = document.querySelectorAll(".pclick");
 
 for (var i = 0; i < pClicks.length; i++) {
   (function (index) {
     pClicks[i].onclick = function () {
-      pItems.forEach(item => item.classList.remove("showme"));
+      pItems.forEach(function (item) {
+        return item.classList.remove("showme");
+      });
       pItems[index].classList.add("showme");
-      pItems[index].firstChild.firstChild.style.display = "block";
-
-      var c = pItems[index].firstChild.children;
-      var k;
-      for (k = 1; k <= c.length; k++) {
-        c[k].style.display = "none";
-        if (k == c.length) {}
-      };
-
+      pItems[index].firstChild.firstChild.classList.add("slideme");
     };
   })(i);
 }
-
 
 /* Slider stuff */
 
@@ -67,18 +61,16 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  //var x = document.getElementsByClassName("mySlides");
   var x = event.target.parentNode.firstChild.children;
-  //console.log(x);
 
   if (n > x.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = x.length
+    slideIndex = x.length;
   }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].classList.remove("slideme");
   }
-  x[slideIndex - 1].style.display = "block";
+  x[slideIndex - 1].classList.add("slideme");
 }
